ee056c4c10506ea29d5fee969d56c3b21646b878
Nafilahaulia <aulianafilah718@gmail.com>
2025-09-13 12:05:46 +0700

    Add files via upload

---
+++ KELAS X/Video/Web Dasar/Episode 41 - 52 [Javascript]/javascript.js
@@ -0,0 +1,73 @@
+function tampil() { 
+//   document.querySelector("#h1").innerText = "Belajar javascript";
+//   document.querySelector(".container").style.backgroungColor = "red"
+//   document.querySelector("#para").style.fontSize = "50px";
+
+// console.log (document.querySelectorAll("img"));
+
+//    min = Math.ceil(0);
+//    max = Math.floor(2);
+//    let n =Math.floor(Math.random()n* (max - min + 1) + min);
+//    console.log(n);
+
+//    document.querySelectorAll("img")[n].style.visibility="visible";
+
+//    alert(document.querySelector("#nama").value);
+
+//    document.querySelector("#hasil").value = document.querySelector("#nama").value;
+
+//    document.querySelector("#para").innerText = document.querySelector("#nama").value;
+
+btn.onlick = function () {
+
+    const nama = document.querySelector(#nama);
+
+    if (nama.value == "a") {
+        alert("nama sama")
+    }else {
+       alert(nama.value) 
+    }
+
+    // > < == != <= >=
+
+}
+//    let a = 3;
+//    let b = 2;
+
+//    b = b + a;
+//    b = b - a;
+//    b = b * a;
+//    b = b / a;
+//    b = a ++ b;
+
+//    console.log(b);
+
+}
+
+// btn.onclick = tampil;
+
+// btn.onclick = function () {
+//  alert("javascript");
+// }
+
+// btn.addEventListener('mouseover',tampil);
+
+//btn.addEventListener('mouseover',function () {
+//    alert("event");
+//});
+
+//btn.onclick = function () {
+//    let menu = document.querySelector("ul");
+
+//    menu.removeChild(menu.children[0]);
+
+// const nama = document.querySelector("#nama");
+// const judul = document.querySelector("h1");
+
+// let tampil = "belajar";
+
+// judul.innerHTML = tampil + nama.value;
+
+// tampil = "javascript";
+// judul.innerHTML = tampil + nama.value;
+//}
