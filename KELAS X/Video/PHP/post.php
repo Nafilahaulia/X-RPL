ea3727ff5a61b10c836cebaadd52823f13fdd944
Nafilahaulia <aulianafilah718@gmail.com>
2025-09-21 12:19:24 +0700

    Add files via upload

--- KELAS X/Video/PHP/post.php
+++ KELAS X/Video/PHP/post.php
@@ -1,10 +1,10 @@
-<nav>
-    <ul>
-        <li>Kontak</li>
-        <li>Sejarah</li>
-        <li>Jurusan</li>
-    </ul>
-</nav>
+<form action="" method="post">
+    email :
+    <input type="email" name="email">
+    password :
+    <input type="password" name="password">
+    <input type="submit" name="kirim" value="login">
+</form>
 
 <?php
 
