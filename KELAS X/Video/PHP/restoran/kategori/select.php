6a58d13dbfd1cc324587f4e74ca85a645ddf9cd9
Nafilahaulia <aulianafilah718@gmail.com>
2025-09-28 20:13:05 +0700

    Add files via upload

--- KELAS X/Video/PHP/restoran/kategori/select.php
+++ KELAS X/Video/PHP/restoran/kategori/select.php
@@ -1,4 +1,52 @@
+<?php
+    $jumlahdata = $db->rowCOUNT("SELECT idkategori FROM tblkategori");
+    $banyak = 4;
 
-<h1> select kategori</h1>
+    $halaman = ceil ($jumlahdata / $banyak);
 
-<a href="?f=kategori&m=insert">Tambah Data</a>
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+        $mulai = ($p * $banyak) - $banyak;
+    
+    }else {
+         $mulai =0;
+    }
+
+    $sql = "SELECT * FROM tblkategori ORDER BY kategori ASC LIMIT $mulai,$banyak";
+    $row = $db->getALL($sql);
+
+    $no=1+$mulai;
+?>
+<h3>Kategori</h3>
+<table class="table table-bordered w-50">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Kategori</th>
+            <th>Delete</th>
+            <th>Update</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach($row as $r): ?>
+        <tr>
+            <td><?php echo $no++ ?></td>
+            <td><?Php echo $r['kategori'] ?></td>
+            <td><?Php echo $r['idkategori'] ?></td>
+            <td><?Php echo $r['idkategori'] ?></td>
+        </tr>
+        <?php endforeach ?>
+    </tbody>
+
+</table>
+
+<?php
+
+    for ($i=1; $i <= $halaman ; $i++) {
+        echo '<a href="?f=kategori&m=select&p='.$i.'">'.$i.'</a>'.$i;
+        echo '&nbsp &nbsp &nbsp';
+    } 
+
+
+?>
+
