7095ca6bc33008d30dc28226951c3f43216e4404
Nafilahaulia <aulianafilah718@gmail.com>
2026-02-26 07:52:32 +0700

    Add files via upload
    Pak Tri

---
+++ KELAS X/Kelas/23-FEBRUARI-2025/MuridRpl.java
@@ -0,0 +1,6 @@
+
+public class MuridRpl {
+  String nama="lalay";
+  int absen=10;
+  int usia=17;
+}
---
+++ KELAS X/Kelas/23-FEBRUARI-2025/SepuluhRpl.java
@@ -0,0 +1,13 @@
+
+public class SepuluhRpl {
+
+   
+    public static void main(String[] args) {
+        MuridRpl objek= new MuridRpl();
+        System.out.println(objek.nama);
+        System.out.println("Nama Anda adalah: "+ objek);
+        objek.nama="yaya";
+        System.out.println("Nama anda adalah: "+ objek);
+    }
+    
+}
