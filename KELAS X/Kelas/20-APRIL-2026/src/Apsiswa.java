3ed0f72c580d2be26e1dc21bc8e4acbd2f1a6ede
Nafilahaulia <aulianafilah718@gmail.com>
2026-04-20 09:31:43 +0700

    Add files via upload
    Pak Tri

---
+++ KELAS X/Kelas/20-APRIL-2026/src/Apsiswa.java
@@ -0,0 +1,15 @@
+
+public class Apsiswa {
+
+    
+    public static void main(String[] args) {
+        Siswa objek= new Siswa();
+        System.out.println("");
+        Siswa objek1= new Siswa("Nafilah");
+        System.out.println("");
+        Siswa objek2= new Siswa("Ilham",16);
+        System.out.println("");
+        Siswa objek3= new Siswa("Abe", 17, "Sidokerto");
+    }
+    
+}
---
+++ KELAS X/Kelas/20-APRIL-2026/src/pkg20/april/pkg2026/APRIL2026.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg20.april.pkg2026;
+
+/**
+ *
+ * @author hp 14s-dk0023au
+ */
+public class APRIL2026 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
